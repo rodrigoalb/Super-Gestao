1f1f7a547e28818b5fcf21eb9911974a1dcc8a4e
rodrigoalbaz <rodrigoalba-@outlook.com>
2023-05-08 18:42:02 -0300

    Rotas contato agora está disponível para receber parâmetros

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,16 @@ Route::get('/', [MainController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [ContatoController::class, 'contato']);
+
+//Rota que estará recebendo parâmetros: Nome, Categoria, Assunto, Mensagem, Estado (opcional)
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}/{estado?}',
+ function (
+    string $nome,
+    string $categoria,
+    string $assunto,
+    string $mensagem,
+    string $estado = 'Estado não informado.'
+    ) {
+        echo "Olá $nome, o motivo do seu contato é $categoria, Assunto: $assunto, Sua mensagem: $mensagem Estado: $estado";
+});
+
