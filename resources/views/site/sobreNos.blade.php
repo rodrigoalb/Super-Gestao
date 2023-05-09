34f7f1ccb078b88512e6316793bc8a68f0fceaad
rodrigoalbaz <rodrigoalba-@outlook.com>
2023-05-08 21:29:56 -0300

    Melhorias e novas implementações nas rotas e views.

--- resources/views/site/sobreNos.blade.php
+++ resources/views/site/sobreNos.blade.php
@@ -1,1 +1,7 @@
 <h3>Sobre Nós (view)</h3>
+
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre Nós</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,41 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [MainController::class, 'principal']);
-
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
-
 Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/login', function (){return "login";});
 
-//Rota que estará recebendo parâmetros: Nome, Categoria, Assunto, Mensagem, Estado (opcional)
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}/{estado?}',
- function (
-    string $nome,
-    string $categoria,
-    string $assunto,
-    string $mensagem,
-    string $estado = 'Estado não informado.'
-    ) {
-        echo "Olá $nome, o motivo do seu contato é $categoria, Assunto: $assunto, Sua mensagem: $mensagem Estado: $estado";
+//Agrupando rotas para separar rotas públicas de privadas
+Route::prefix('/app')->group(function (){
+    Route::get('/clientes', function (){return "Clientes";});
+    Route::get('/fornecedores', function (){return "Fornecedores";});
+    Route::get('/produtos',  function (){return "Produtos";});
 });
 
+
+//Exemplos de direcionamento de rotas
+Route::get('/rota1', function (){
+    echo "Rota1";
+});
+
+Route::get('/rota2', function (){
+    //return redirect()->route('/rota1'); //Caso tivesse um name na rota, poderia ser implementado desse jeito
+});
+Route::redirect('/rota2', 'rota1');
+
+//Caso o usuário tente acessar qualquer rota inexistente no sistema
+Route::fallback(function(){
+    echo 'A página que você está tentando acessar não existe, <a href="/">clique aqui</a> para retornar para a página inicial.';
+});
+
+/*
+//Rota que estará recebendo parâmetros: Nome, Categoria, Assunto, Mensagem, Estado (opcional)
+Route::get('/contato/{nome}/{categoria_id?}',
+ function (
+    string $nome,
+    int $categoria_id = 1
+    ) {
+        echo "Olá $nome, o motivo do seu contato é $categoria_id";
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');  //Validação com expressão regular (RegEx)
+*/
